125d832b6fa79d390f1598ed8a91004435671b7d
safiwork101-dotcom <safiwork101@gmail.com>
2026-05-22 12:04:08 +0500

    Show generator on checkout route

---
+++ app/checkout/page.tsx
@@ -0,0 +1,12 @@
+import { SiteShell } from "@/components/SiteShell";
+import { GeneratorSection } from "@/components/sections/GeneratorSection";
+
+export default function CheckoutPage() {
+  return (
+    <SiteShell>
+      <div className="pt-28">
+        <GeneratorSection />
+      </div>
+    </SiteShell>
+  );
+}
